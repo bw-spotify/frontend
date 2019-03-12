fdf86f8191d00f0b515e0adcb57327d05cdb587a
JaySebagh <jaysebagh@gmail.com>
2019-03-11 17:28:58 -0700

    changed data to api info and displayed more data

--- spotify/src/components/Song.js
+++ spotify/src/components/Song.js
@@ -12,9 +12,22 @@ class Song extends Component {
   render() {
     return (
         <div>
-            <p className="trackInfo">Song Name: {this.props.trackInfo}</p>
-            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>{song.track_id}</p>) : (null)) }</div>
-            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>{song.track_id}</p>) : (null)) }</div>
+            <p className="trackInfo">{this.props.trackInfo}</p>
+            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>Artist: {song.artist_name}</p>) : (null)) }</div>
+            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>Acousticness: {song.acousticness}</p>) : (null)) }</div>
+            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>Danceability: {song.danceability}</p>) : (null)) }</div>
+            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>Duration in ms: {song.duration_ms}</p>) : (null)) }</div>
+            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>Energy: {song.energy}</p>) : (null)) }</div>
+            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>Instrumentalness: {song.instrumentalness}</p>) : (null)) }</div>
+            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>Key: {song.key}</p>) : (null)) }</div>
+            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>Liveness: {song.liveness}</p>) : (null)) }</div>
+            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>Loudness: {song.loudness}</p>) : (null)) }</div>
+            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>Mode: {song.mode}</p>) : (null)) }</div>
+            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>Speechiness: {song.speechiness}</p>) : (null)) }</div>
+            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>Tempo: {song.tempo}</p>) : (null)) }</div>
+            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>Time Signature: {song.time_signature}</p>) : (null)) }</div>
+            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>Valence: {song.valence}</p>) : (null)) }</div>
+            <div className="trackID">{ this.props.songsArray.map((song, i) => this.props.trackInfo === song.track_name ? (<p key={i}>Popularity: {song.popularity}</p>) : (null)) }</div>
         </div>
       );
     }
